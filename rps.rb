1cef5a5731791c3ac646e2260ea71e8e954c2989
Christopher Brum <christopher.brum@gmail.com>
2021-10-19 10:02:42 -0700

    adjusted code to satisfy some rubocop infractions, pre-move classes

--- rps.rb
+++ rps.rb
@@ -1,5 +1,5 @@
 class Move
-  CHOICES = ['rock', 'paper', 'scissors']
+  CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
   def initialize(value)
     @value = value
@@ -17,16 +17,38 @@ class Move
     @value == 'scissors'
   end
 
+  def lizard?
+    @value == 'lizard'
+  end
+
+  def spock?
+    @value == 'spock'
+  end
+
   def >(other_move)
-    (rock? &&  other_move.scissors?) ||
+    (rock? && other_move.scissors?) ||
+      (rock? && other_move.lizard?) ||
+      (paper? && other_move.spock?) ||
       (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?)
+      (scissors? && other_move.paper?) ||
+      (scissors? && other_move.lizard?) ||
+      (lizard? && other_move.spock?) ||
+      (lizard? && other_move.paper?) ||
+      (spock? && other_move.scissors?) ||
+      (spock? && other_move.rock?)
   end
 
   def <(other_move)
     (rock? && other_move.paper?) ||
+      (rock? && other_move.spock?) ||
       (paper? && other_move.scissors?) ||
-      (scissors? && other_move.rock?)
+      (paper? && other_move.lizard?) ||
+      (scissors? && other_move.rock?) ||
+      (scissors? && other_move.spock?) ||
+      (lizard? && other_move.rock?) ||
+      (lizard? && other_move.scissors?) ||
+      (spock? && other_move.paper?) ||
+      (spock? && other_move.lizard?)
   end
 
   def to_s
@@ -34,13 +56,40 @@ class Move
   end
 end
 
+class Rock
+  def beats?
+    
+  end
+end
+
+class Paper
+end
+
+class Scissor
+end
+
+class Lizard
+end
+
+class Spock
+end
+
 class Player
   COMPUTER_NAMES = ['C3PO', 'Wall-e', 'Robo-Bob', 'Johnny-5']
 
-  attr_accessor :move, :name
+  attr_accessor :move, :name, :points
 
   def initialize
+    @points = 0
     set_name
+  end
+
+  def add_point
+    @points += 1
+  end
+
+  def reset_points
+    @points = 0
   end
 end
 
@@ -60,7 +109,7 @@ class Human < Player
   def choose
     choice = nil
     loop do
-      puts "Please choose rock, paper, or scissors:"
+      puts "Please choose rock, paper, scissors, lizard or spock:"
       choice = gets.chomp.downcase
       break if Move::CHOICES.include? choice
       puts "Invalid choice"
@@ -80,24 +129,65 @@ class Computer < Player
 end
 
 class RPSGame
-  attr_reader :human, :computer
+  POINTS_TO_WIN = 3
+
+  attr_reader :human, :computer, :score
 
   def initialize
     @human = Human.new
     @computer = Computer.new
   end
 
+  def clear
+    system("clear")
+  end
+
+  def display_blank_line
+    puts ''
+  end
+
+  def pause
+    sleep(4)
+  end
+
   def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors,  #{human.name}!"
+    puts "Welcome to Rock, Paper, Scissors, Lizard, Spock, #{human.name}!"
+    display_blank_line
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors #{human.name}!. "
+    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock,
+      #{human.name}!"
   end
 
   def display_moves
+    display_blank_line
     puts "#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
+    display_blank_line
+  end
+
+  def update_score
+    if human.move > computer.move
+      human.add_point
+    elsif human.move < computer.move
+      computer.add_point
+    end
+  end
+
+  def format_score(player)
+    if human.points == 1
+      puts "#{player.name} has #{player.points} point"
+    else
+      puts "#{player.name} has #{player.points} points"
+    end
+  end
+
+  def display_score
+    display_blank_line
+    format_score(human)
+    format_score(computer)
+    display_blank_line
   end
 
   def display_winner
@@ -108,6 +198,9 @@ class RPSGame
     else
       puts "It's a tie!"
     end
+
+    update_score
+    display_score
   end
 
   def play_again?
@@ -120,18 +213,45 @@ class RPSGame
       puts "Sorry, must be y or n."
     end
 
-    return false if answer == 'n'
-    return true if answer == 'y'
+    answer == 'y'
   end
 
-  def play
-    display_welcome_message
+  def winner?
+    human.points >= POINTS_TO_WIN || computer.points >= POINTS_TO_WIN
+  end
+
+  def display_grand_winner
+    if human.points >= POINTS_TO_WIN
+      puts "#{human.name} is the GRAND WINNER!!!"
+    else
+      puts "#{computer.name} is the GRAND WINNER!!!"
+    end
+    display_blank_line
+  end
+
+  def reset_game
+    human.reset_points
+    computer.reset_points
+  end
+
+  def turn
     loop do
       human.choose
       computer.choose
       display_moves
       display_winner
+      break if winner?
+    end
+  end
+
+  def play
+    clear
+    display_welcome_message
+    loop do
+      turn
+      display_grand_winner
       break unless play_again?
+      reset_game
     end
     display_goodbye_message
   end
