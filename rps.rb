71fa962de6a8a3d8fb17c801cf28011a7fd2b5c5
Christopher Brum <christopher.brum@gmail.com>
2021-10-19 15:39:56 -0700

    4/5 bonus features

--- rps.rb
+++ rps.rb
@@ -1,87 +1,80 @@
 class Move
   CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
-  def initialize(value)
-    @value = value
-  end
-
-  def rock?
-    @value == 'rock'
-  end
-
-  def paper?
-    @value == 'paper'
-  end
-
-  def scissors?
-    @value == 'scissors'
-  end
-
-  def lizard?
-    @value == 'lizard'
-  end
-
-  def spock?
-    @value == 'spock'
-  end
+  attr_reader :type, :wins, :loses
 
   def >(other_move)
-    (rock? && other_move.scissors?) ||
-      (rock? && other_move.lizard?) ||
-      (paper? && other_move.spock?) ||
-      (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?) ||
-      (scissors? && other_move.lizard?) ||
-      (lizard? && other_move.spock?) ||
-      (lizard? && other_move.paper?) ||
-      (spock? && other_move.scissors?) ||
-      (spock? && other_move.rock?)
+    wins.include?(other_move.type)
   end
 
   def <(other_move)
-    (rock? && other_move.paper?) ||
-      (rock? && other_move.spock?) ||
-      (paper? && other_move.scissors?) ||
-      (paper? && other_move.lizard?) ||
-      (scissors? && other_move.rock?) ||
-      (scissors? && other_move.spock?) ||
-      (lizard? && other_move.rock?) ||
-      (lizard? && other_move.scissors?) ||
-      (spock? && other_move.paper?) ||
-      (spock? && other_move.lizard?)
-  end
-
-  def to_s
-    @value
+    loses.include?(other_move.type)
   end
 end
 
-class Rock
-  def beats?
-    
+class Rock < Move
+  def initialize
+    @type = 'rock'
+    @wins = ['lizard', 'scissors']
+    @loses = ['paper', 'spock']
   end
 end
 
-class Paper
+class Paper < Move
+  def initialize
+    @type = 'paper'
+    @wins = ['rock', 'spock']
+    @loses = ['scissors', 'lizard']
+  end
 end
 
-class Scissor
+class Scissor < Move
+  def initialize
+    @type = 'scissors'
+    @wins = ['paper', 'lizard']
+    @loses = ['spock', 'rock']
+  end
 end
 
-class Lizard
+class Lizard < Move
+  def initialize
+    @type = 'lizard'
+    @wins = ['spock', 'paper']
+    @loses = ['rock', 'scissors']
+  end
 end
 
-class Spock
+class Spock < Move
+  def initialize
+    @type = 'spock'
+    @wins = ['scissors', 'rock']
+    @loses = ['lizard', 'paper']
+  end
 end
 
 class Player
   COMPUTER_NAMES = ['C3PO', 'Wall-e', 'Robo-Bob', 'Johnny-5']
 
-  attr_accessor :move, :name, :points
+  attr_accessor :name, :points, :move, :move_history
 
   def initialize
     @points = 0
-    set_name
+    @name = name_prompt
+    @move_history = []
+  end
+
+  def update_move_history(move)
+    move_history << move.type
+  end
+
+  def find_move_type(value)
+    current_move = Rock.new if value == 'rock'
+    current_move = Paper.new if value == 'paper'
+    current_move = Scissor.new if value == 'scissors'
+    current_move = Lizard.new if value == 'lizard'
+    current_move = Spock.new if value == 'spock'
+    update_move_history(current_move)
+    current_move
   end
 
   def add_point
@@ -94,13 +87,13 @@ class Player
 end
 
 class Human < Player
-  def set_name
+  def name_prompt
     n = ''
     loop do
+      system('clear')
       puts "What's your name?"
-      n = gets.chomp.capitalize
+      n = gets.chomp.strip.capitalize
       break unless n.empty?
-
       puts "Sorry, must enter a value"
     end
     self.name = n
@@ -110,21 +103,22 @@ class Human < Player
     choice = nil
     loop do
       puts "Please choose rock, paper, scissors, lizard or spock:"
-      choice = gets.chomp.downcase
+      choice = gets.chomp.strip.downcase
       break if Move::CHOICES.include? choice
       puts "Invalid choice"
     end
-    self.move = Move.new(choice)
+
+    self.move = find_move_type(choice)
   end
 end
 
 class Computer < Player
-  def set_name
+  def name_prompt
     self.name = COMPUTER_NAMES.sample
   end
 
   def choose
-    self.move = Move.new(Move::CHOICES.sample)
+    self.move = find_move_type(Move::CHOICES.sample)
   end
 end
 
@@ -147,7 +141,10 @@ class RPSGame
   end
 
   def pause
-    sleep(4)
+    6.times do
+      print '. '
+      sleep(0.65)
+    end
   end
 
   def display_welcome_message
@@ -156,14 +153,13 @@ class RPSGame
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock,
-      #{human.name}!"
+    puts "Thanks for playing #{human.name}, Goodbye!"
   end
 
   def display_moves
     display_blank_line
-    puts "#{human.name} chose #{human.move}"
-    puts "#{computer.name} chose #{computer.move}"
+    puts "#{human.name} chose #{human.move.type}."
+    puts "#{computer.name} chose #{computer.move.type}."
     display_blank_line
   end
 
@@ -188,6 +184,12 @@ class RPSGame
     format_score(human)
     format_score(computer)
     display_blank_line
+    display_wins_needed
+    display_blank_line
+  end
+
+  def display_wins_needed
+    puts "First player to #{POINTS_TO_WIN}, wins!"
   end
 
   def display_winner
@@ -208,7 +210,7 @@ class RPSGame
 
     loop do
       puts "Would you like to play again? (y/n)"
-      answer = gets.chomp.downcase
+      answer = gets.chomp.strip.downcase
       break if ['y', 'n'].include? answer
       puts "Sorry, must be y or n."
     end
@@ -240,8 +242,31 @@ class RPSGame
       computer.choose
       display_moves
       display_winner
+      pause
+      clear
       break if winner?
     end
+  end
+
+  def display_history
+    display_blank_line
+    human.move_history.each_with_index do |current_move, index|
+      puts "Move #{index + 1}; #{current_move}"
+    end
+    display_blank_line
+  end
+
+  def move_history?
+    clear
+    answer = nil
+    loop do
+      puts "Would you like to see all the moves you made? (y/n)"
+      answer = gets.chomp.strip.downcase
+      break if ['y', 'n'].include? answer
+      puts "Sorry, must be y or n."
+    end
+
+    display_history if answer == 'y'
   end
 
   def play
@@ -253,6 +278,7 @@ class RPSGame
       break unless play_again?
       reset_game
     end
+    move_history?
     display_goodbye_message
   end
 end
